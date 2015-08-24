2ebb89a90b92bd82171f257896b9167888309cd2
kraljo <aljosa@mordenkul.com>
2015-08-24 23:12:07 +0200

    3 del

---
+++ DUCK UNDER/Assets/PostaviElementeSkripta.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PostaviElementeSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject jablana;
+	public GameObject drevo;
+	public GameObject smreka;
+
+	public GameObject nazaj;
+	Bounds bounds;
+	List<bool> list;
+	GameObject[] tabela;
+	void Start () {
+		tabela = new GameObject[3];
+		tabela [0] = jablana;
+		tabela [1] = drevo;
+		tabela [2] = smreka;
+		Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+		bounds = mesh.bounds;
+		Bounds jabkaSize = jablana.GetComponent<MeshFilter> ().sharedMesh.bounds;
+
+		int velikostX = (int)((bounds.size.x * transform.localScale.x) / (jabkaSize.size.x * jablana.transform.localScale.x));
+		int velikostZ = (int)((bounds.size.z * transform.localScale.z) / (jabkaSize.size.z * jablana.transform.localScale.z));
+
+		float zamikX = transform.position.x - (bounds.size.x * transform.localScale.x / 2);
+		float zamikZ = transform.position.z - (bounds.size.z * transform.localScale.z / 2);
+
+		for (int i=0; i < 50; i++) {
+			float x = Random.Range(0,velikostX)*jabkaSize.size.x * jablana.transform.localScale.x;
+			float z = Random.Range(0,velikostZ)*jabkaSize.size.z * jablana.transform.localScale.z;
+			GameObject otrok = Instantiate(tabela[Random.Range(0,tabela.Length)],new Vector3(x+zamikX,0,z+zamikZ), Quaternion.Euler(0,0,0)) as GameObject;
+			otrok.transform.SetParent(transform);
+			
+		}
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ DUCK UNDER/Assets/PovoziRacoSkripta.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PovoziRacoSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	RackaSkripta skripta;
+	void Start () {
+		skripta = transform.parent.GetComponent<RackaSkripta> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals ("kolo")) {
+			skripta.povoziRaco ();
+		}
+	}
+}
