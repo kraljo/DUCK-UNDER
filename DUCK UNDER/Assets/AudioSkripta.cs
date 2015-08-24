f0feb9236cc81aa6274bcb15821a1ed8df13f105
kraljo <aljosa@mordenkul.com>
2015-08-24 22:07:11 +0200

    1 del

---
+++ DUCK UNDER/Assets/AudioSkripta.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject raca;
+	public AudioClip povozena;
+	AudioSource source;
+	void Start () {
+		raca = GameObject.Find ("raca");
+		source = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = raca.transform.position;
+		transform.rotation = raca.transform.rotation;
+	}
+
+	public void povozi(){
+		if (!source.isPlaying) {
+			source.clip = povozena;
+			source.Play();
+		}
+	}
+}
