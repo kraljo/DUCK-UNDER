590b87fccc5f9773764d8d79475304e8ee7c94e8
kraljo <aljosa@mordenkul.com>
2015-08-24 23:36:05 +0200

    10 del

---
+++ DUCK UNDER/Assets/SencaRotacijaSkripta.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SencaRotacijaSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	public static float rot=0;
+	void Start () {
+		transform.rotation = Quaternion.Euler (0,rot,0);
+	}
+
+}
---
+++ DUCK UNDER/Assets/proba.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class proba : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreLayerCollision(8,8, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals ("voda")) {
+			Debug.Log("dela kolider ampak nocem");
+		}
+	}
+}
